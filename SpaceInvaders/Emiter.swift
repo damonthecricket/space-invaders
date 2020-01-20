3cdcd11859a5ac78fca14c435d2952c4f51468c0
Damon Cricket <d.a.tsvirko@gmail.com>
2020-01-20 13:08:40 +0200

    Add player, enemies.

---
+++ SpaceInvaders/Emiter.swift
@@ -0,0 +1,51 @@
+//
+//  Emiter.swift
+//  SpaceInvaders
+//
+//  Created by Damon Cricket on 20.01.2020.
+//  Copyright © 2020 DC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol EmiterDelegate: class {
+    func emiter(_ emiter: Emiter, didMoveView view: UIView)
+}
+
+class Emiter {
+    weak var delegate: EmiterDelegate?
+    
+    var view: UIView? = nil
+    
+    var timer: Timer? = nil
+    
+    var yOffset: CGFloat = 0.0
+    
+    var timeInterval: TimeInterval = 0.0
+    
+    deinit {
+        view = nil
+        stopTimer()
+    }
+    
+    func start() {
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(timerTick(timer:)), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func timerTick(timer: Timer) {
+        view?.frame = CGRect(x: view!.frame.minX, y: view!.frame.minY - yOffset, width: view!.frame.width, height: view!.frame.height)
+        delegate?.emiter(self, didMoveView: view!)
+    }
+    
+    func stop() {
+        stopTimer()
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+}
---
+++ SpaceInvaders/MainViewController.swift
@@ -0,0 +1,116 @@
+//
+//  MainViewController.swift
+//  SpaceInvaders
+//
+//  Created by Damon Cricket on 20.01.2020.
+//  Copyright © 2020 DC. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, EmiterDelegate {
+    struct Constants {
+        struct Player {
+            static let width: CGFloat = 35.0
+            static let height: CGFloat = 80.0
+        }
+        struct Fire {
+            static let size = Constants.Player.width
+        }
+    }
+    
+    @IBOutlet weak var blindView: UIView?
+    
+    @IBOutlet weak var startButton: UIButton?
+    
+    let playerView = UIView()
+    
+    let fireView = UIView()
+    
+    var blockViews = [UIView]()
+    
+    var isFire: Bool = false
+    
+    let emiter = Emiter()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startButton?.layer.borderColor = UIColor(red: 0/255, green: 122/255, blue: 255/255, alpha: 1.0).cgColor
+        startButton?.layer.borderWidth = 3.0
+        startButton?.layer.cornerRadius = 3.0
+
+        let playerX = (view.bounds.width - Constants.Player.width)/2.0
+        let playerY = view.bounds.height - Constants.Player.height
+        playerView.frame = CGRect(x: playerX, y: playerY, width: Constants.Player.width, height: Constants.Player.height)
+        playerView.backgroundColor = .black
+        view.addSubview(playerView)
+        
+        fireView.backgroundColor = .red
+        view.addSubview(fireView)
+        
+        emiter.view = fireView
+        emiter.yOffset = 1.0
+        emiter.delegate = self
+        emiter.timeInterval = 0.0005
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizer(recognizer:)))
+        view.addGestureRecognizer(panGestureRecognizer)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGestureRecognizer(recognizer:)))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func panGestureRecognizer(recognizer: UIPanGestureRecognizer) {
+        let location = recognizer.location(in: view)
+        playerView.center = CGPoint(x: location.x, y: playerView.center.y)
+        if playerView.frame.minX < 0.0 {
+            playerView.frame = CGRect(x: 0.0, y: playerView.frame.minY, width: Constants.Player.width, height: Constants.Player.height)
+        } else if playerView.frame.minX > view.bounds.width {
+            playerView.frame = CGRect(x: view.bounds.width - Constants.Player.width, y: playerView.frame.minY, width: Constants.Player.width, height: Constants.Player.height)
+        }
+    }
+    
+    @objc func tapGestureRecognizer(recognizer: UITapGestureRecognizer) {
+        if !isFire {
+            fireView.frame = CGRect(x: playerView.frame.minX, y: playerView.frame.minY - Constants.Fire.size, width: Constants.Fire.size, height: Constants.Fire.size)
+            emiter.start()
+            isFire = true
+        }
+    }
+    
+    func emiter(_ emiter: Emiter, didMoveView v: UIView) {
+        if v.frame.minY < -v.frame.width {
+            emiter.stop()
+            isFire = false
+        }
+    }
+    
+    @IBAction func startButtonTap(sender: UIButton) {
+        blindView?.isHidden = true
+        placeBlocks()
+    }
+    
+    func placeBlocks() {
+        for v in blockViews {
+            v.removeFromSuperview()
+        }
+        blockViews.removeAll()
+        var lastX: CGFloat = 0.0
+        var lastY: CGFloat = UIApplication.shared.statusBarFrame.height
+        for _ in 0 ..< 5 {
+            let count = 5
+            lastX = 0.0
+            for _ in 0 ... count - 1 {
+                let blockWidth = view.bounds.width / CGFloat(count)
+                let blockView = UIView(frame: CGRect(x: lastX, y: lastY , width: blockWidth, height: 20.0))
+                lastX = blockView.frame.maxX
+                blockView.backgroundColor = .blue
+                blockViews.append(blockView)
+                view.addSubview(blockView)
+            }
+            lastY = blockViews.last!.frame.maxY
+        }
+
+    }
+}
